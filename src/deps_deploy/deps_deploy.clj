c71451fc8533d5397aff3d96d2c4dd387639c6b1
Rick Moynihan <rick.m@swirrl.com>
2020-11-04 13:03:14 +0000

    Add support for deploying to private S3 buckets.  Closes #17.

--- src/deps_deploy/deps_deploy.clj
+++ src/deps_deploy/deps_deploy.clj
@@ -4,7 +4,14 @@
             [clojure.edn :as edn]
             [clojure.pprint :as pp]
             [clojure.java.io :as io]
-            [clojure.data.xml :as xml]))
+            [clojure.data.xml :as xml])
+  (:import [org.springframework.build.aws.maven
+            PrivateS3Wagon SimpleStorageServiceWagon]))
+
+
+(aether/register-wagon-factory! "s3p" #(PrivateS3Wagon.))
+(aether/register-wagon-factory! "s3" #(SimpleStorageServiceWagon.))
+
 
 (def default-repo-settings {"clojars" {:url (or (System/getenv "CLOJARS_URL") "https://clojars.org/repo")
                                        :username (System/getenv "CLOJARS_USERNAME")
