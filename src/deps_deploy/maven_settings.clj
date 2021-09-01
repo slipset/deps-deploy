a38898f26c55b3dbdae8ddc45e0c82fa50689ea9
Eugen Stan <eugen.stan@netdava.com>
2021-09-01 11:25:24 +0300

    Added multi arity - call fn's without any params

--- src/deps_deploy/maven_settings.clj
+++ src/deps_deploy/maven_settings.clj
@@ -29,20 +29,24 @@
 (defn ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity read-settings-security
   "Reads settings-security.xml file into an ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity object.
    Defaults to $HOME/.m2/settings-security.xml ."
-  [settings-security-path]
-  (let [settings-security-path (or settings-security-path default-settings-security-path)]
-    (SecUtil/read (.getAbsolutePath (io/as-file settings-security-path)) true)))
+  ([]
+   (read-settings-security nil))
+  ([settings-security-path]
+   (let [settings-security-path (or settings-security-path default-settings-security-path)]
+     (SecUtil/read (.getAbsolutePath (io/as-file settings-security-path)) true))))
 
 (defn ^org.apache.maven.settings.Settings read-settings
   "Reads settings.xml file into a ^org.apache.maven.settings.Settings object.
    Defaults to $HOME/.m2/settings.xml.
 
    See https://maven.apache.org/ref/3.6.2/maven-settings/apidocs/org/apache/maven/settings/Settings.html"
-  [settings-path]
-  (let [settings-path (or settings-path default-settings-path)
-        sr (SettingsXpp3Reader.)]
-    (with-open [rdr (io/reader (io/as-file settings-path))]
-      (.read sr rdr))))
+  ([]
+   (read-settings nil))
+  ([settings-path]
+   (let [settings-path (or settings-path default-settings-path)
+         sr (SettingsXpp3Reader.)]
+     (with-open [rdr (io/reader (io/as-file settings-path))]
+       (.read sr rdr)))))
 
 (defn decode-server-password
   "Decodes the server passowr, given a ^org.apache.maven.settings.Server and plain master password."
@@ -63,48 +67,56 @@
 (defn servers-with-passwords
   "Decodes the passwords from servers.
    Returns a map from server id -> server settings (including credentials)."
-  [^org.apache.maven.settings.Settings settings
-   ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity settings-security]
-  (let [encoded-master-pw (.getMaster settings-security)
-        plain-master-pw (decode-master-password encoded-master-pw)
-        servers (.getServers settings)
-        map-server (map-server-factory plain-master-pw)]
-    (into {} (map map-server servers))))
+  ([]
+   (servers-with-passwords (read-settings) (read-settings-security)))
+  ([^org.apache.maven.settings.Settings settings
+    ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity settings-security]
+   (let [encoded-master-pw (.getMaster settings-security)
+         plain-master-pw (decode-master-password encoded-master-pw)
+         servers (.getServers settings)
+         map-server (map-server-factory plain-master-pw)]
+     (into {} (map map-server servers)))))
 
 (defn active-profiles
   "Map of active profile name to ^org.apache.maven.settings.Profile instance."
-  [^org.apache.maven.settings.Settings settings]
-  (let [active-profiles (.getActiveProfiles settings)
-        profiles-map (.getProfilesAsMap settings)]
-    (select-keys profiles-map active-profiles)))
+  ([]
+   (active-profiles (read-settings)))
+  ([^org.apache.maven.settings.Settings settings]
+   (let [active-profiles (.getActiveProfiles settings)
+         profiles-map (.getProfilesAsMap settings)]
+     (select-keys profiles-map active-profiles))))
 
 (defn active-repositories
   "Returns a list of active repositories from settings.xml active profiles.
    Does not include crededentials."
-  [^org.apache.maven.settings.Settings settings]
-  (let [active-profiles (active-profiles settings)
-        get-repos (fn get-repos [p] (into [] (.getRepositories (val p))))
-        repo2props (fn repo2props [r] {(.getId r) {:id (.getId r)
-                                                   :url (.getUrl r)
-                                                   :name (.getName r)
-                                                   :layout (.getLayout r)
-                                                   :_repository r}})
-        repos (flatten (map get-repos active-profiles))]
-    (into {} (map repo2props repos))))
+  ([]
+   (active-repositories (read-settings)))
+  ([^org.apache.maven.settings.Settings settings]
+   (let [active-profiles (active-profiles settings)
+         get-repos (fn get-repos [p] (into [] (.getRepositories (val p))))
+         repo2props (fn repo2props [r] {(.getId r) {:id (.getId r)
+                                                    :url (.getUrl r)
+                                                    :name (.getName r)
+                                                    :layout (.getLayout r)
+                                                    :_repository r}})
+         repos (flatten (map get-repos active-profiles))]
+     (into {} (map repo2props repos)))))
 
 (defn deps-repositories
   "Returns a map of repo id -> repository settings for easy consumption by deps-deploy.
    Repositories are read from settings.xml.
    Passwords for each server are decoded and added to each repo."
-  [opts]
-  (let [{:keys [settings settings-security]} opts
-        settings (or settings default-settings-path)
-        settings-security (or settings-security default-settings-security-path)
-        settings (read-settings (io/as-file settings))
-        settings-security (read-settings-security (io/as-file settings-security))
-        servers-with-pw (servers-with-passwords settings settings-security)
-        active-repos (active-repositories settings)]
-    (merge-with merge servers-with-pw active-repos)))
+  ([]
+   (deps-repositories nil))
+  ([opts]
+   (let [{:keys [settings settings-security]} opts
+         settings (or settings default-settings-path)
+         settings-security (or settings-security default-settings-security-path)
+         settings (read-settings (io/as-file settings))
+         settings-security (read-settings-security (io/as-file settings-security))
+         servers-with-pw (servers-with-passwords settings settings-security)
+         active-repos (active-repositories settings)]
+     (merge-with merge servers-with-pw active-repos))))
 
 
 (comment
@@ -121,14 +133,15 @@
         (println "Username :" (.getUsername s))
         (println "Password :" plain-pw))))
 
-  (servers-with-passwords nil nil)
+  (servers-with-passwords)
 
-  (.getR)(read-settings default-settings-path)
+  (read-settings default-settings-path)
 
-  (active-profiles (read-settings nil))
+  (active-profiles (read-settings))
 
-  (active-repositories (read-settings nil))
+  (active-repositories (read-settings))
 
-  (deps-repositories nil)
+  (deps-repositories)
 
-0)
+  0
+  )
