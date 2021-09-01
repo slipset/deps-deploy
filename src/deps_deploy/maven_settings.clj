a6f09d3f8053ddaa21ae3e099c8a859e92aa55e9
Eugen Stan <eugen.stan@netdava.com>
2021-09-01 12:23:40 +0300

    Fixed code review change requests and added docs in intro

--- src/deps_deploy/maven_settings.clj
+++ src/deps_deploy/maven_settings.clj
@@ -3,85 +3,79 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str])
   (:import [org.apache.maven.settings.io.xpp3 SettingsXpp3Reader]
-           [org.sonatype.plexus.components.cipher
-            DefaultPlexusCipher]
+           [org.apache.maven.settings
+            Server
+            Settings]
+           [org.sonatype.plexus.components.cipher DefaultPlexusCipher]
            [org.sonatype.plexus.components.sec.dispatcher
             DefaultSecDispatcher
-            SecUtil]))
+            SecUtil]
+           [org.sonatype.plexus.components.sec.dispatcher.model SettingsSecurity]))
 
 (def default-settings-path (str (System/getProperty "user.home") "/.m2/settings.xml"))
 (def default-settings-security-path (str (System/getProperty "user.home") "/.m2/settings-security.xml"))
 
 ;; Adapted from https://github.com/jelmerk/maven-settings-decoder/blob/master/src/main/java/org/github/jelmerk/maven/settings/Decoder.java
 
-(defn ^String decode-password
+(defn decode-password
   "Decodes a password (got from settings.xml)."
-  [^String encoded-password
-   ^String key]
+  ^String [^String encoded-password ^String key]
   (-> (DefaultPlexusCipher.)
       (.decryptDecorated encoded-password key)))
 
-(defn ^String decode-master-password
-  "Decodes master password (like the one found in settings-security.xml)."
-  [^String encoded-master-password]
-  (decode-password encoded-master-password DefaultSecDispatcher/SYSTEM_PROPERTY_SEC_LOCATION))
-
-(defn ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity read-settings-security
-  "Reads settings-security.xml file into an ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity object.
+(defn read-settings-security
+  "Reads settings-security.xml file into an ^SettingsSecurity object.
    Defaults to $HOME/.m2/settings-security.xml ."
-  ([]
+  (^SettingsSecurity []
    (read-settings-security nil))
-  ([settings-security-path]
+  (^SettingsSecurity [settings-security-path]
    (let [settings-security-path (or settings-security-path default-settings-security-path)]
      (SecUtil/read (.getAbsolutePath (io/as-file settings-security-path)) true))))
 
-(defn ^org.apache.maven.settings.Settings read-settings
-  "Reads settings.xml file into a ^org.apache.maven.settings.Settings object.
-   Defaults to $HOME/.m2/settings.xml.
-
-   See https://maven.apache.org/ref/3.6.2/maven-settings/apidocs/org/apache/maven/settings/Settings.html"
-  ([]
+(defn read-settings
+  "Reads settings.xml file into a ^Settings object.
+   Defaults to $HOME/.m2/settings.xml."
+  (^Settings []
    (read-settings nil))
-  ([settings-path]
+  (^Settings [settings-path]
    (let [settings-path (or settings-path default-settings-path)
          sr (SettingsXpp3Reader.)]
      (with-open [rdr (io/reader (io/as-file settings-path))]
        (.read sr rdr)))))
 
 (defn decode-server-password
-  "Decodes the server passowr, given a ^org.apache.maven.settings.Server and plain master password."
-  [^org.apache.maven.settings.Server server
-   ^String plain-master-pw]
+  "Decodes the server passowr, given a ^Server and plain master password."
+  ^String [^String plain-master-pw ^Server server]
   (decode-password (.getPassword server) plain-master-pw))
 
-(defn map-server-factory
-  "Returns a function that can map a ^org.apache.maven.settings.Server to a map with decoded password."
-  [plain-master-pw]
-  (fn map-server
-    [server]
-    {(.getId server) {:id (.getId server)
-                      :username (.getUsername server)
-                      :password (decode-password (.getPassword server) plain-master-pw)
-                      :_server server}}))
+(defn server-credentials
+  "Returns a map from server id -> server settings including decoded password and ^Server instance."
+  [plain-master-pw server]
+  (let [id (.getId server)
+        username (.getUsername server)
+        decoded-pw (decode-password (.getPassword server) plain-master-pw)]
+    {id {:id id
+         :username username
+         :password decoded-pw
+         :_server server}}))
 
 (defn servers-with-passwords
   "Decodes the passwords from servers.
    Returns a map from server id -> server settings (including credentials)."
   ([]
    (servers-with-passwords (read-settings) (read-settings-security)))
-  ([^org.apache.maven.settings.Settings settings
-    ^org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity settings-security]
+  ([^Settings settings
+    ^SettingsSecurity settings-security]
    (let [encoded-master-pw (.getMaster settings-security)
-         plain-master-pw (decode-master-password encoded-master-pw)
-         servers (.getServers settings)
-         map-server (map-server-factory plain-master-pw)]
-     (into {} (map map-server servers)))))
+         plain-master-pw (decode-password encoded-master-pw DefaultSecDispatcher/SYSTEM_PROPERTY_SEC_LOCATION)
+         servers (.getServers settings)]
+     (into {} (map (partial server-credentials plain-master-pw) servers)))))
 
 (defn active-profiles
   "Map of active profile name to ^org.apache.maven.settings.Profile instance."
   ([]
    (active-profiles (read-settings)))
-  ([^org.apache.maven.settings.Settings settings]
+  ([^Settings settings]
    (let [active-profiles (.getActiveProfiles settings)
          profiles-map (.getProfilesAsMap settings)]
      (select-keys profiles-map active-profiles))))
@@ -91,7 +85,7 @@
    Does not include crededentials."
   ([]
    (active-repositories (read-settings)))
-  ([^org.apache.maven.settings.Settings settings]
+  ([^Settings settings]
    (let [active-profiles (active-profiles settings)
          get-repos (fn get-repos [p] (into [] (.getRepositories (val p))))
          repo2props (fn repo2props [r] {(.getId r) {:id (.getId r)
@@ -107,14 +101,9 @@
    Repositories are read from settings.xml.
    Passwords for each server are decoded and added to each repo."
   ([]
-   (deps-repositories nil))
-  ([opts]
-   (let [{:keys [settings settings-security]} opts
-         settings (or settings default-settings-path)
-         settings-security (or settings-security default-settings-security-path)
-         settings (read-settings (io/as-file settings))
-         settings-security (read-settings-security (io/as-file settings-security))
-         servers-with-pw (servers-with-passwords settings settings-security)
+   (deps-repositories (read-settings) (read-settings-security)))
+  ([settings settings-security]
+   (let [servers-with-pw (servers-with-passwords settings settings-security)
          active-repos (active-repositories settings)]
      (merge-with merge servers-with-pw active-repos))))
 
@@ -124,7 +113,7 @@
   (let [settings (read-settings (io/as-file default-settings-path))
         settings-security (read-settings-security (io/as-file default-settings-security-path))
         encoded-master-pw (.getMaster settings-security)
-        plain-master-pw (decode-master-password encoded-master-pw)
+        plain-master-pw (decode-password encoded-master-pw DefaultSecDispatcher/SYSTEM_PROPERTY_SEC_LOCATION)
         servers (.getServers settings)]
     (doseq [s servers]
       (let [plain-pw (decode-password (.getPassword s) plain-master-pw)]
@@ -143,5 +132,4 @@
 
   (deps-repositories)
 
-  0
-  )
+  0)
